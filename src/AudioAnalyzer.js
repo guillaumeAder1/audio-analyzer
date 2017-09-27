29ed13c29f516b48103ab7d277da9c3a3b0b76c5
guillaumeAder1 <guillaume.ader1@gmail.com>
2017-09-27 21:17:08 +0100

    fix analyzer with local file, trace the freq value in drawing loop

--- src/AudioAnalyzer.js
+++ src/AudioAnalyzer.js
@@ -5,27 +5,28 @@ export default class _AudioAnalyzer {
 
     constructor(params) {
         console.log("constructor analyser init...", params);
-        this.frequencies = null;
+        this.frequencies = "toto";
     }
     init() {
         let player = document.createElement('audio');
         player.classList.add('player-audio');
         player.src = './assets/techno.mp3';
+        //player.loop = true;
         player.play();
         let context = new AudioContext();
         let source = context.createMediaElementSource(player);
-        let analyser = context.createAnalyser();
-        analyser.fftSize = 64;
-        source.connect(analyser);
-        analyser.connect(context.destination);
-        this.frequencies = new Uint8Array(analyser.frequencyBinCount);
-
-        requestAnimationFrame(this.draw)
-
+        this.analyser = context.createAnalyser();
+        this.analyser.fftSize = 64;
+        source.connect(this.analyser);
+        this.analyser.connect(context.destination);
+        this.frequencies = new Uint8Array(this.analyser.frequencyBinCount);
+        this.draw()
         document.body.appendChild(player);
     }
 
     draw() {
+        this.analyser.getByteFrequencyData(this.frequencies)
         console.log('drawing...', this.frequencies)
+        window.requestAnimationFrame(this.draw.bind(this))
     }
 }
