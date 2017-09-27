e2d00bbbb82c956d37a1b157aaae4c46c0a61b1f
Guillaume Ader <GAder@esriuk.com>
2017-09-27 17:44:36 +0100

    plug audio analyser

--- src/AudioAnalyzer.js
+++ src/AudioAnalyzer.js
@@ -2,16 +2,30 @@
 
 export default class _AudioAnalyzer {
 
+
     constructor(params) {
-
         console.log("constructor analyser init...", params);
-
+        this.frequencies = null;
     }
     init() {
-        var player = document.createElement('audio');
+        let player = document.createElement('audio');
         player.classList.add('player-audio');
         player.src = './assets/techno.mp3';
         player.play();
+        let context = new AudioContext();
+        let source = context.createMediaElementSource(player);
+        let analyser = context.createAnalyser();
+        analyser.fftSize = 64;
+        source.connect(analyser);
+        analyser.connect(context.destination);
+        this.frequencies = new Uint8Array(analyser.frequencyBinCount);
+
+        requestAnimationFrame(this.draw)
+
         document.body.appendChild(player);
+    }
+
+    draw() {
+        console.log('drawing...', this.frequencies)
     }
 }
